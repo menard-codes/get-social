1e6f8ec083379ac34a98b39c2b08211e468f923c
Menard Maranan <66345147+menard-codes@users.noreply.github.com>
2022-05-12 12:56:50 +0800

    developed TopBar component UI - #21

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,6 @@ export default function Home() {
 					Welcome to <a href='https://nextjs.org'>Next.js!</a>
 				</h1>
 
-				<TopBar username='Menard Maranan' photoUrl='/Slide1.jpg' />
-
 				<p className={styles.description}>
 					Get started by editing{' '}
 					<code className={styles.code}>pages/index.js</code>
