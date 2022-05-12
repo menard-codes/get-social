ac5e4a8cbc39044dc73feeac97e86142d363a283
Menard Maranan <66345147+menard-codes@users.noreply.github.com>
2022-05-12 12:29:51 +0800

    developed Chatroom component UI - #20

---
+++ components/inbox/Chatroom.jsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+export default function Chatroom({
+	photoUrl,
+	username,
+	messagePreview,
+	time,
+	chatroomURL,
+}) {
+	// TODO: There should be a messagePreview truncation functionality here
+	return (
+		<a
+			href={chatroomURL}
+			className='grid grid-cols-[1fr_3fr] w-full hover:bg-slate-100 p-3 rounded-lg'
+		>
+			<div className='flex items-center'>
+				<Image
+					src={photoUrl}
+					alt='avatar'
+					width='60'
+					height='60'
+					className='rounded-full'
+				/>
+			</div>
+			<div>
+				<div className='flex justify-between'>
+					<p className='font-black'>{username}</p>
+					<time className='text-[15px] text-slate-600'>{time}</time>
+				</div>
+				<p className='text-sm text-slate-600'>{messagePreview}</p>
+			</div>
+		</a>
+	);
+}
